77c3791a7df028ce5039c835fd62c4e6e33277a6
myselfsagar <ssahu6244@gmail.com>
2025-02-28 11:03:04 +0530

    added getElementsByClassName Method

--- fruit.js
+++ fruit.js
@@ -1,3 +1,4 @@
+// -----------------Understanding DOM & getElementById Method------------------------------
 let mainHeading = document.getElementById("main-heading");
 mainHeading.textContent = "Fruit World";
 mainHeading.style.color = "orange";
@@ -11,3 +12,10 @@ basketHeading.style.color = "green";
 
 let thanks = document.getElementById("thanks");
 thanks.innerHTML = "<p>Please visit us again</p>";
+
+// -----------------getElementsByClassName Method------------------------------
+const fruit = document.getElementsByClassName("fruit");
+fruit[2].style.backgroundColor = "yellow";
+for (let i = 0; i < fruit.length; i++) {
+  fruit[i].style.fontWeight = "bold";
+}
