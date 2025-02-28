9106cbf792de73e2f0f1d82fb7b80727ab6d4799
myselfsagar <ssahu6244@gmail.com>
2025-02-28 11:00:38 +0530

    fruit file added

---
+++ fruit.js
@@ -0,0 +1,13 @@
+let mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+
+let header = document.getElementById("header");
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+
+let basketHeading = document.getElementById("basket-heading");
+basketHeading.style.color = "green";
+
+let thanks = document.getElementById("thanks");
+thanks.innerHTML = "<p>Please visit us again</p>";
