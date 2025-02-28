bc4ff270e7a06ab40e02d43134dcc913190b13ce
myselfsagar <ssahu6244@gmail.com>
2025-02-28 12:18:53 +0530

    added querySelector & querySelectorAll Methods

--- fruit.js
+++ fruit.js
@@ -1,4 +1,5 @@
 // -----------------Understanding DOM & getElementById Method------------------------------
+
 let mainHeading = document.getElementById("main-heading");
 mainHeading.textContent = "Fruit World";
 mainHeading.style.color = "orange";
@@ -14,6 +15,7 @@ let thanks = document.getElementById("thanks");
 thanks.innerHTML = "<p>Please visit us again</p>";
 
 // -----------------getElementsByClassName Method------------------------------
+
 const fruit = document.getElementsByClassName("fruit");
 fruit[2].style.backgroundColor = "yellow";
 for (let i = 0; i < fruit.length; i++) {
@@ -21,8 +23,50 @@ for (let i = 0; i < fruit.length; i++) {
 }
 
 // -----------------getElementsByTagName Method------------------------------
+
 const liItems = document.getElementsByTagName("li");
 liItems[4].style.color = "blue";
 for (let i = 0; i < liItems.length; i++) {
   liItems[i].style.fontStyle = "italic";
+}
+
+// ----------------querySelector & querySelectorAll Methods-------------------
+
+// Write the code as shown in the video below:
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.textAlign = "center";
+
+const fruits = document.querySelector(".fruits");
+fruits.style.backgroundColor = "gray";
+fruits.style.padding = "30px";
+fruits.style.margin = "30px";
+fruits.style.width = "50%";
+fruits.style.borderRadius = "5px";
+fruits.style.listStyleType = "none";
+
+let basketHeading = document.querySelector("h2");
+basketHeading.style.marginLeft = "30px";
+
+// QuerySelectorAll is used to grab all the elements with same class/tag
+const fruitItems = document.querySelectorAll(".fruit");
+for (let i = 0; i < fruitItems.length; i++) {
+  fruitItems[i].style.backgroundColor = "white";
+  fruitItems[i].style.padding = "10px";
+  fruitItems[i].style.margin = "10px";
+  fruitItems[i].style.borderRadius = "5px";
+}
+
+const oddFruitItems = document.querySelectorAll(".fruit:nth-child(odd)");
+for (let i = 0; i < oddFruitItems.length; i++) {
+  oddFruitItems[i].style.backgroundColor = "lightgray";
+}
+
+// Write answer to the questions asked below:
+basketHeading = document.querySelector("#basket-heading");
+basketHeading.style.color = "brown";
+
+const evenFruitItems = document.querySelectorAll(".fruit:nth-child(even)");
+for (let i = 0; i < evenFruitItems.length; i++) {
+  evenFruitItems[i].style.backgroundColor = "brown";
+  evenFruitItems[i].style.color = "white";
 }
